b2fdd1719951d787f2b4210676fda840c70c3c86
Aed1988 <aed1988@users.noreply.github.com>
2019-10-26 17:20:15 +0100

    Added express-validation to validate data sent

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check,  validationResult } = require('express-validator')
+
+const Users = require('../models/User');
 
 // @route   POST /api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user')
+router.post('/', [
+  check('name', 'Please add name').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password must be at least 6 characters long').isLength({ min:6 })
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  res.send('passed');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect DB
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => { res.json( {msg: 'Welcome to the contact keeper API.'} ) })
 
 // Define Routes
@@ -13,6 +16,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
