c1354def94278f78e74886a597dde1c1b6f977cc
Aed1988 <aed1988@users.noreply.github.com>
2019-11-03 19:55:22 +0000

    56- Set & Clear Current Contact completed

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -7,12 +7,13 @@ import { faEnvelopeOpen, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const ContactItem = ({contact}) => {
   const contactContext = useContext(ContactContext);
-  const { deleteContact } = contactContext;
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
   const { name, id, email, phone, type } = contact;
 
   const onDelete = () => {
     deleteContact(id);
+    clearCurrent();
   }
 
   return (
@@ -39,7 +40,7 @@ const ContactItem = ({contact}) => {
         )}
       </ul>
       <p>
-        <button class='btn btn-dark btn-sm'>Edit</button>
+        <button class='btn btn-dark btn-sm' onClick={() => setCurrent(contact)}>Edit</button>
         <button class='btn btn-danger btn-sm' onClick={onDelete}>Delete</button>
       </p>
     </div>
